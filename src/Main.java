de835a9804e8826eb77856b934171aa1a4950e4a
Alexandra Santos <aleseect@gmail.com>
2025-04-14 20:43:20 -0300

    Calcula a média de duas matérias e retorna se foi aprovada, reprovovada ou está em recuperação

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,32 @@ public class Main {
         //calcula a media de 2 materias
         //informa se foi aprovado, reprovado ou esta de recuperação
 
-        
+        String primeiraAluna = "Alexandra";
+        String segundaAluna = "Ravena";
 
-        );
+        //matérias
+        double notaFilosofia;
+        double notaBiologia;
+        double mediaGeral = 7;
+
+        System.out.println("Olá, bem vinda ao sistema.");
+
+        //notas estaticas
+        notaFilosofia = 7.5;
+        notaBiologia = 7.0;
+
+        double mediaAluna = (notaFilosofia + notaBiologia)/2;
+
+        if(mediaAluna >= mediaGeral){
+
+            System.out.println("A aluna " + primeiraAluna + ", tem uma média de " + mediaAluna + " e ela foi aprovada!");
+
+        } else if(mediaAluna >= 5 ){
+            System.out.println("Ficou de recuperação, com média " + mediaAluna);
+        }else{
+            System.out.println("A aluna " + primeiraAluna + ", tem uma média de " + mediaAluna + " e ela foi reprovada.");
+        }
+
+
     }
 }
