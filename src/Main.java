0ee39967359dca1819b1d36f7015e92b7942c135
Alexandra Santos <aleseect@gmail.com>
2025-04-14 20:15:08 -0300

    Início do Projeto

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //pega o nome do aluno
+        //calcula a media de 2 materias
+        //informa se foi aprovado, reprovado ou esta de recuperação
+
+        
+
+        );
+    }
+}
